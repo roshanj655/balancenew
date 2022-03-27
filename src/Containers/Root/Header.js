13cada62bb3465ecd42ae158b0137e98f770e00a
Roshan Jha <roshanj655@gmail.com>
2022-03-27 10:47:59 +0530

    notification added

--- src/Containers/Root/Header.js
+++ src/Containers/Root/Header.js
@@ -1,6 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MagicBell, { FloatingNotificationInbox } from "@magicbell/magicbell-react";
+
 function Header(props) {
+  const theme = {"icon":{"borderColor":"#a9a1a6","width":"20px","width":"20px"},"unseenBadge":{"backgroundColor":"#DF4759"},"header":{"backgroundColor":"#d2866d","textColor":"#ffffff","borderRadius":"16px"},"footer":{"backgroundColor":"#d2866d","textColor":"#ffffff","borderRadius":"16px"},"notification":{"default":{"textColor":"#15091F","borderRadius":"8px","backgroundColor":"#d2866d"},"unseen":{"backgroundColor":"#6113A3"},"unread":{"backgroundColor":"#6113A3"}}};
+
     return (
 <nav className="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+
         <div className="dark-bg text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
           
         </div>
@@ -42,10 +49,16 @@ function Header(props) {
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
-                <i className="mdi mdi-bell-outline"></i>
-                <span className="count-symbol bg-danger"></span>
+                <MagicBell
+                apiKey="8476498a6627f7e5c15a2507cbb299e54a170abc"
+                userEmail="mark.naufel@faraya.org"
+                theme={theme}
+                locale="en"
+              >
+                {(props) => <FloatingNotificationInbox width={400} height={500} {...props} />}
+              </MagicBell>
               </a>
-              <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+              {/* <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
                 <h6 className="p-3 mb-0">Notifications</h6>
                 <div className="dropdown-divider"></div>
                 <a className="dropdown-item preview-item">
@@ -85,7 +98,7 @@ function Header(props) {
                 </a>
                 <div className="dropdown-divider"></div>
                 <h6 className="p-3 mb-0 text-center">See all notifications</h6>
-              </div>
+              </div> */}
             </li>
             <li className="nav-item nav-profile dropdown">
               <a className="nav-link dropdown-toggle" id="profileDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
